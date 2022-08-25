8734513be46af485b34950c471f1bdaefa960d95
EmanuelG-Gaming <emanuelgidea2000@gmail.com>
2022-08-25 10:45:06 +0300

    FOW rendering

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,7 @@ let lineLength = 20 * Vars.tilesize;
 let FOV = 20;
 let spacing = Vars.tilesize / 2;
 
+let player;
 let enabled = false;
 
 function initFrag() {
@@ -36,16 +37,18 @@ function initFrag() {
 
 function Ray(unit, angle) {
    let len;
-   World.raycast(tile(unit.x), tile(unit.y), tile(unit.x + Angles.trnsx(angle, lineLength)), tile(unit.y + Angles.trnsy(angle, lineLength)), (tx, ty) => {
-      let t = Vars.world.tile(tx, ty);
-      if (t != null && t.solid()) {
-      	//TODO no tile snapping?
-      	len = unit.dst(t);
-      	return true;
-      }
-      len = lineLength;
-      return false;
-   });
+   if (!unit.isFlying()) { //do not block the rays coming from flying units
+      World.raycast(tile(unit.x), tile(unit.y), tile(unit.x + Angles.trnsx(angle, lineLength)), tile(unit.y + Angles.trnsy(angle, lineLength)), (tx, ty) => {
+         let t = Vars.world.tile(tx, ty);
+         if (t != null && t.solid()) {
+        	len = unit.dst(t); //TODO no tile snapping?
+        	return true;
+         }
+         len = lineLength;
+         return false;
+     });
+   }
+   else len = lineLength;
    return len;
 };
 
@@ -53,18 +56,20 @@ function tile(number) {
    return World.toTile(number);
 };
 
+function checkLights() {
+   return (enabled && Vars.renderer.lights.enabled());
+};
+
 function drawRays() {
-   let plr = Vars.player.unit();
-   let angle = plr.rotation;
-   
+   let angle = player.rotation;
    for (let i = 0; i < FOV; i++) {
       let angleThing = angle - (FOV * (spacing / 2)) / 2 + i * (spacing / 2);
-      let r = Ray(plr, angleThing);
+      let r = Ray(player, angleThing);
       Drawf.light(
-         plr.x, plr.y,
-         plr.x + Angles.trnsx(angleThing, r),
-         plr.y + Angles.trnsy(angleThing, r),
-         spacing, plr.type.lightColor, plr.type.lightOpacity
+         player.x, player.y,
+         player.x + Angles.trnsx(angleThing, r),
+         player.y + Angles.trnsy(angleThing, r),
+         spacing, player.type.lightColor, player.type.lightOpacity
       );
    }
 };
@@ -73,17 +78,41 @@ Events.on(ClientLoadEvent, () => {
    //no need for circular lanterns here
    Vars.content.units().each(u => u.lightRadius = 0);
    
+   //experimental fog of war rendering
+   Vars.content.units().each(u => {
+   	let prev = u.constructor.get();
+   	u.constructor = () => extend(prev.getClass(), {
+   	    inFogTo(team) {
+   	       if (this == player) return false;
+   
+   	       //totally avant stuff here
+       	   let temp = Angles.angle(player.x, player.y, this.x, this.y);
+              let tempDst = Mathf.dst(player.x, player.y, this.x, this.y);
+              if (Vars.renderer.lights.enabled()) {
+                 if (this.team != team) {
+                    if (enabled && tempDst <= lineLength && Angles.within(player.rotation, temp, (FOV * spacing + 0.75) / 2)) {
+                   	return false;
+    	            }
+                 }
+                 else return false;
+                 return true;
+              }
+              return false;
+   	    },
+       });
+   });
+  
    Vars.ui.settings.addCategory("Mindustry Raycaster", Icon.units, tb => {
       tb.sliderPref("Field Of View", 20, 1, 35, 1, res => {
-      	FOV = parseInt(res);
+     	FOV = parseInt(res);
 
-      	return res;
+     	return res;
       });
 
       tb.sliderPref("Line Length", 20, 1, 35, 1, res => {
-      	lineLength = parseInt(res) * Vars.tilesize;
+     	lineLength = parseInt(res) * Vars.tilesize;
 
-      	return res;
+     	return res;
       });
    });
    
@@ -93,8 +122,9 @@ Events.on(ClientLoadEvent, () => {
 
 Events.run(Trigger.draw, () => {
    //only light when there is lighting, the flashlight is on, and the player isn't a building
-   if (Vars.state.isMenu() || !Vars.renderer.lights.enabled()) return;
-   if (!enabled || Vars.player.unit() instanceof BlockUnitc) return;
+   if (Vars.state.isMenu() || !checkLights() || player instanceof BlockUnitc) return;
    
    drawRays();
 });
+
+Events.run(Trigger.update, () => player = Vars.player.unit());
